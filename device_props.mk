7d69869c553d8d6a8abe6c1f4ec9662d2e8902ed
kdrag0n <danny@kdrag0n.dev>
2021-07-20 11:24:33 +0530

    potter : Enable compositor background blurring

--- device_props.mk
+++ device_props.mk
@@ -60,6 +60,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.bluetooth.map=true \
     persist.bt.enableAptXHD=true \
 
+# Blurs
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.supports_background_blur=1
+
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.hw.fm.init=0
